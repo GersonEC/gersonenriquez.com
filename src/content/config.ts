7263a0def9c14ec72a032219bdedd915e320faa8
GersonEC <gersonenriquez94@gmail.com>
2024-02-04 10:21:05 +0100

    set up articles content collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,9 +1,12 @@
-// 1. Import utilities from `astro:content`
-import { defineCollection } from 'astro:content';
-// 2. Define your collection(s)
-const blogCollection = defineCollection({ /* ... */ });
-// 3. Export a single `collections` object to register your collection(s)
-//    This key should match your collection directory name in "src/content"
+import { z, defineCollection } from 'astro:content'
+
+const articlesCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+  }),
+})
+
 export const collections = {
-  'blog': blogCollection,
-};
+  articles: articlesCollection,
+}
--- src/pages/blog/first.md
+++
@@ -1,6 +0,0 @@
----
-layout: '../../layouts/Layout.astro'
-title: 'First blog post'
----
-
-Awesome!
