32fa7358ce5eac70eab952f9c3e3f7bcf25b2091
GersonEC <gersonenriquez94@gmail.com>
2024-02-16 18:25:39 +0100

    add btn primary tailwind class

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,18 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			'darkblue': '#13151a',
+			'primary-dark': '#deb887',
+			'pink': '#ff49db',
+			'orange': '#ff7849',
+			'green': '#13ce66',
+			'gray-dark': '#273444',
+			'gray': '#8492a6',
+			'gray-light': '#d3dce6',
+		},
 		extend: {},
 	},
+	darkMode: 'class',
 	plugins: [],
 }
