d1c7a23d1ce432ab68c30b5b24a3e3e482ca253d
GersonEC <gersonenriquez94@gmail.com>
2024-04-18 11:55:21 +0200

    replace layout and tailwind config and global css files

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		colors: {
+			dark: {
+				DEFAULT: 'hsl(210deg, 30%, 8%)',
+			},
 			gray: {
 				DEFAULT: '#6b7280',
 				light: '#9ca3af',
@@ -18,6 +21,8 @@ export default {
 				light: '#fef08a',
 				dark: '#eab308',
 			},
+			white: '#ffffff',
+			black: '#000000',
 		},
 		extend: {},
 	},
