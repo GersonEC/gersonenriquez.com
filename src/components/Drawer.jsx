049bce602b63581a49c5b46188122d0d3ba117a9
GersonEC <gersonenriquez94@gmail.com>
2025-02-05 16:07:22 +0100

    style drawer elements and height homepage mobile

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -10,38 +10,38 @@ const Drawer = () => {
   return (
     <div className=' absolute top-4 right-4'>
       {isOpen ? (
-        <div className=' h-screen w-screen z-10 bg-dark flex flex-col justify-start p-8 rounded'>
+        <div className='h-screen w-screen z-10 bg-dark flex flex-col justify-start p-8 rounded'>
           <div title='Close' onClick={handleOpen}>
             X
           </div>
-          <div className='flex flex-col pt-2'>
+          <div className='flex flex-col items-center gap-4'>
             <a
               href='/now'
-              className='mr-5 text-lg hover:brightness-75 transition duration-200 ease-out-in'
+              className='mr-5 text-3xl hover:brightness-75 transition duration-200 ease-out-in'
             >
               Now
             </a>
             <a
               href='/projects'
-              className='mr-5 text-lg hover:brightness-75 transition duration-200 ease-out-in'
+              className='mr-5 text-3xl hover:brightness-75 transition duration-200 ease-out-in'
             >
               Projects
             </a>
             <a
               href='/articles/'
-              className='mr-5 text-lg hover:brightness-75 transition duration-200 ease-out-in'
+              className='mr-5 text-3xl hover:brightness-75 transition duration-200 ease-out-in'
             >
               Articles
             </a>
             <a
               href='/booknotes/'
-              className='mr-5 text-lg hover:brightness-75 transition duration-200 ease-out-in'
+              className='mr-5 text-3xl hover:brightness-75 transition duration-200 ease-out-in'
             >
               Book Notes
             </a>
             <a
               href='/cv/'
-              className=' text-lg hover:brightness-75 transition duration-200 ease-out-in'
+              className=' text-3xl hover:brightness-75 transition duration-200 ease-out-in'
             >
               CV
             </a>
