39efa7e9d9ce0695ffabb557eba041babb72e328
GersonEC <gersonenriquez94@gmail.com>
2025-02-13 17:46:01 +0100

    fix drawer and link book notes to notion page

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -1,4 +1,4 @@
-import react, { useState } from 'react';
+import { useState } from 'react';
 
 const Drawer = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,10 +11,14 @@ const Drawer = () => {
     <div className=' absolute top-4 right-4'>
       {isOpen ? (
         <div className='h-screen w-screen z-10 bg-dark flex flex-col justify-start p-8 rounded'>
-          <div title='Close' onClick={handleOpen}>
+          <div
+            className='flex justify-end font-semibold text-2xl'
+            title='Close'
+            onClick={handleOpen}
+          >
             X
           </div>
-          <div className='flex flex-col items-center gap-4'>
+          <div className='flex flex-col gap-4 mt-4'>
             <a
               href='/now'
               className='mr-5 text-3xl hover:brightness-75 transition duration-200 ease-out-in'
@@ -34,8 +38,9 @@ const Drawer = () => {
               Articles
             </a>
             <a
-              href='/booknotes/'
+              href='https://gersonenriquez.notion.site/7ebc6a676a864aff8de38c36d79b6674?v=62b4e41b54c54ae38c52b750d051cd4b'
               className='mr-5 text-3xl hover:brightness-75 transition duration-200 ease-out-in'
+              target='_blank'
             >
               Book Notes
             </a>
